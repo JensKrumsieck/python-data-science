268baf6bd8ff440b58e9099680e7cbeccff0a786
Jens Krumsieck <jenso666@live.de>
2021-12-30 18:11:17 +0100

    Ep 15.

--- np_tricks.py
+++ np_tricks.py
@@ -6,30 +6,35 @@ arr = np.ones(42)
 arr = np.zeros(42)
 
 arr = np.arange(-42, 69, 2, dtype=('i2'))  # via step
-#
-# [-42 -40 -38 -36 -34 -32 -30 -28 -26 -24 -22 -20 -18 -16 -14 -12 -10  -8
-#  -6  -4  -2   0   2   4   6   8  10  12  14  16  18  20  22  24  26  28
-#  30  32  34  36  38  40  42  44  46  48  50  52  54  56  58  60  62  64
-#  66  68]
-#
-
 arr = np.linspace(-42, 1337.2, 5)  # via len
-# [ -42.   302.8  647.6  992.4 1337.2]
-
 arr = np.full((2, 3), 69)  # fill matrix
-#
-# [[69 69 69]
-# [69 69 69]]
-#
-
 arr = np.eye(3, dtype='i1')  # identity matrix
-#
-# [[1 0 0]
-# [0 1 0]
-# [0 0 1]]
-#
-
 arr = np.random.random((2, 3))  # random float 2x3 matrix
-
 arr = np.empty((2, 3))  # weird empty matrix
-print(arr)
+
+# OPERATIONS
+V = np.arange(-42, 1337, 42)
+M = np.eye(42)
+x = np.subtract(V, V)  # add, multiply, exp, ...
+
+x = np.sin(V)  # cos, tan, acos, ...
+
+x = np.exp(V)  # e^V
+x = np.exp2(V)  # 2^V
+x = np.power(V, 2)  # V^2
+
+x = np.log(V)  # ln(V)
+x = np.log10(V)  # log(10)
+x = np.log2(V)  # log2(V)
+
+x = np.argmax(V)  # 32  = index at max
+x = np.argmin(V)  # 0  = index at min
+x = np.max(V)  # 1302 = max value
+V = np.random.randint(0, 10, size=(10,))
+x = np.argsort(V)  # sort indices by vector values ASC
+
+idx = [x[0]] + [x[0], x[1], x[2]]
+np.add.at(V, idx, 1)  # ufunc, V is modified
+print(V)
+print(x)
+print(idx)
