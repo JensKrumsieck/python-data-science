bb1d4fe99c41ba71ec874ccb5129093401acc85a
Jens Krumsieck <jenso666@live.de>
2021-12-30 10:54:40 +0100

    Ep. 3

--- main.py
+++ main.py
@@ -5,3 +5,24 @@ np_temp = np.array(temp)
 
 print(3 * np_temp)
 print(np.array([42, 127], np.int8))
+
+dt = np.dtype("i4") # i4 -> int32
+print(dt.itemsize)
+print(dt.name)
+print(dt.type is np.int32)
+
+print(dt.byteorder) # = -> nativ, < -> little, > -> big endian, | -> not applicable
+# big endian 1000 = 0*2^0 + 0*2^1 + 0*2^2 + 1*2^3 = 8 (vereinfacht, eigentlich bitorder)
+# little endian 1000 = 1*2^0 + 0*2^1 + 0*2^2 + 0*2^3 = 1
+
+dt = np.dtype("?") # boolean
+print(dt.type)
+
+dt = np.dtype("f8") # float64
+print(dt.type)
+
+dt = np.dtype("c16") # complex128
+print(dt.type)
+
+dt = np.dtype("U") # unicode str
+print(dt.type)
