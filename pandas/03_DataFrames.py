8690c44c88d006fad0c1a52f2a69c92b79ac632d
Jens Krumsieck <jenso666@live.de>
2022-01-02 14:03:26 +0100

    pd Ep 5

--- pandas/03_DataFrames.py
+++ pandas/03_DataFrames.py
@@ -18,3 +18,16 @@ print(df.iloc[3])
 B = df.pop("B")  # "Wenn wir Poppen..." - Morpheus, 20.07.2021
 print(df)
 print(df.assign(hans=lambda x: x["A"] + x["C"]))
+print(df/5)  # divide by 5
+print(df.sub(2))  # subtract 2
+print(df.add(df.iloc[1]))  # add first row to all
+print(df.add(df.iloc[1], axis=0))  # INVALID!
+print(df.head())  # 1st five rows
+print(df.tail())  # last five rows
+print(df.tail(1))  # last row
+print(df.fillna("LOL"))  # fills NaNs
+print(df.eq(df))  # true frame, if NaN = false!
+print((df > 0).all())  # für ganze spalten
+print((df > 0).any())
+# df.combine_first(df2) -> packe alle leeren in df mit df2 werten voll
+print(df.to_numpy())  # to np matrix
