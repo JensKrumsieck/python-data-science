8caddd08f4128750437710c488b495c5e5b1cfbe
Jens Krumsieck <jenso666@live.de>
2022-01-02 12:16:04 +0100

    pd Ep. 1

---
+++ pandas/DataFrame.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "id": [0, 1, 2],
+        "name": ["Cedric", "Morpheus", "Morph"],
+        "age": [28, 28, 28]
+    }
+)
+
+print(df)
+print(df["age"].describe())
+print(pd.Series([1,2,3], name="lol"))
