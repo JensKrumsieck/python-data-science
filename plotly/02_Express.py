7f73bf47e53bd470ea545f6d0b6699450e56d5a1
Jens Krumsieck <jenso666@live.de>
2022-01-03 16:48:51 +0100

    plotly ep 4

--- plotly/02_Express.py
+++ plotly/02_Express.py
@@ -5,8 +5,14 @@ df = px.data.iris()  # sample data
 # Virginische Schwertlilie (Iris virginica)
 # Verschiedenfarbige Schwertlilie (Iris versicolor)
 print(df)
+
+df["error"] = df["sepal_width"]/50
+
 # sepal = Kelchblatt, petal = Blütenblatt
 fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
-                 marginal_x="box", marginal_y="violin",  # marginal = Datenbereiche graphisch (Median, Punktverteilung, etc)
-                trendline="ols")
+                 # marginal = Datenbereiche graphisch (Median, Punktverteilung, etc)
+                 marginal_x="box", marginal_y="violin",
+                 trendline="ols",
+                 error_x="error"  # Fehlerbalken, error_y auch existent
+                 )
 fig.show()
