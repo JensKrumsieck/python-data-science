e033d1d4da99e14661b94e5e5c7c682f49e4f58e
Jens Krumsieck <jenso666@live.de>
2022-01-03 16:32:02 +0100

    plotly ep2

--- plotly/01_Intro.py
+++ plotly/01_Intro.py
@@ -1,5 +1,37 @@
 import plotly.graph_objects as go
+import plotly.io as pio
 
-graph = go.Figure(data=go.Bar(y=[42, 1337, 69]))
-graph.write_html("plot1_ignore.html", auto_open=True)
+graph = go.Figure(
+    data=go.Bar(
+        x=[0, 1, 2],
+        y=[42, 1337, 69]
+    ),
+    layout=go.Layout(
+        title=go.layout.Title(text="Beispiel")
+    )
+)
+# graph.write_html("plot1_ignore.html", auto_open=True)
 graph.show()
+
+fig = dict({
+    "layout": {
+        "title": {
+            "text": "Beispiel"
+        },
+    },
+    "data": [
+        {
+            "type": "bar",
+            "x": [0, 1, 2],
+            "y": [42, 1337, 69]
+        }
+    ]
+
+})
+graph_fig = go.Figure(fig)
+graph_fig.show()
+pio.show(fig)
+
+# all 3 figures identical! :)
+
+print(graph.to_dict())
