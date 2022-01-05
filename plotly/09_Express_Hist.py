b3220a6ebcef3d033f126039e0f7341476920d6a
Jens Krumsieck <jenso666@live.de>
2022-01-05 12:20:56 +0100

    plotly ep 13

--- plotly/09_Express_Hist.py
+++ plotly/09_Express_Hist.py
@@ -3,6 +3,10 @@ import numpy as np
 
 df = px.data.tips()
 fig = px.histogram(df, x="total_bill", nbins=10, y="tip", color="day")
+fig = px.histogram(df, x="total_bill", nbins=10,
+                   histnorm="percent")  # probability density
+fig = px.histogram(df, x="total_bill", y="tip",
+                   histfunc="avg", color="sex", pattern_shape="smoker")  # count, avg,
 fig.update_layout(bargap=.1)
 fig.show()
 
@@ -11,4 +15,3 @@ bins = bins[:-1] + bins[1:]
 fig2 = px.bar(x=bins, y=counts)
 fig2.update_layout(bargap=.1)
 fig2.show()
-print(counts)
